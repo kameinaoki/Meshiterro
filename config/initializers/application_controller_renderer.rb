502f4a7e5cffecd39b950ec75ed435068dd3aa92
kameinaoki <ec2-user@ip-172-31-49-148.ec2.internal>
2021-09-15 17:32:18 +0000

    [finish] 4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'a52fecd0e79ab0ea6851ab6a495e845040bb8692395da7dd1e541aa64a0e7d9424c9d1f01922eb8134d61c20d07a223a25ac63e655626eb483506305102b23b4'
